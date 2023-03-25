11a50237199a4fce9b670c29cb4eea5326311bef
Ligia Rocha <ligiarocha@Mac-mini-de-Ligia.local>
2023-03-25 18:16:38 -0300

    fim do curso de Templates e Boas Práticas

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
 # boa prática de programação
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
 ]
